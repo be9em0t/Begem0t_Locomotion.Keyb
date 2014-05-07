956d37a31ec1d3992ac319730fd02aacfcc5f7e7
be9em0t@gmail.com <be9em0t@gmail.com>
2014-05-07 23:41:57 +0300

    Random Idle anims using 2D BlendTree

--- Assets/Demo_MocapiAnimation/Scripts/IdleTest.cs
+++ Assets/Demo_MocapiAnimation/Scripts/IdleTest.cs
@@ -3,41 +3,43 @@ using System.Collections;
 
 public class IdleTest : MonoBehaviour {
 
-    float currentIdle = 0f;
-    float NextIdle;
-    bool NextIdleAllow = true;
+    int NextIdle;               //select next idle animation from a list
+    bool NextIdleAllow = true;  //allow to select next idle animation
+    Vector2[] idleAnimsList;    //array holding Idle animations' vectors in 2D BlendTree
+    Vector2 currentIdleVector;  //current idle animation' vector
+    Vector2 nextIdleVector;     //next idle animation' vector
+    float IdleSmooth = 0.01f;   //how much we want to lerp when transitioning between idle animations
     private Animator anim;
     private AnimatorStateInfo animState;
 
 	// Use this for initialization
 	void Start () {
-        anim = GetComponent<Animator>();	
+        anim = GetComponent<Animator>();
 
+        //describe Idle animations' vectors in 2D BlendTree (you need a corresponding 2D blendtree in Animator Controller)
+        Vector2 idle1 = new Vector2(0f, 0f);
+        Vector2 idle2 = new Vector2(0f, 1f);
+        Vector2 idle3 = new Vector2(-1f, 0.2f);
+        Vector2 idle4 = new Vector2(1f, 0.2f);
+        Vector2 idle5 = new Vector2(-1f, -1f);
+        Vector2 idle6 = new Vector2(1f, -1f);
+        idleAnimsList = new Vector2[] { idle1, idle2, idle3, idle4, idle5, idle6 }; //populate the idle animation's list
     }
 	
 	// Update is called once per frame
 	void Update () {
+        animState = anim.GetCurrentAnimatorStateInfo(0);    // Get our animator's current state. we need this to be know when to switch animations
 
-        animState = anim.GetCurrentAnimatorStateInfo(0);// Get our animator's current state
         IdleVariantsTest();
-
-        currentIdle = Mathf.Lerp(currentIdle, NextIdle, Time.time * 0.001f);
-        anim.SetFloat("IdleState", currentIdle);
-
-        Debug.Log(currentIdle);
-
 	}
 
     void IdleVariantsTest()
     {
-
         int animLoopNum = (int)animState.normalizedTime;
         float animPercent = Mathf.Round(((animState.normalizedTime - animLoopNum) * 100f)) / 100f;     //round to DP2
-        //float animPercent = animState.normalizedTime;
         if ((animPercent > .9f) && (NextIdleAllow == true) )
         {
-            Debug.Log("change!");
-            NextIdle = UnityEngine.Random.Range(0, 5);  //random select next transition
+            NextIdle = UnityEngine.Random.Range(0, 6);  //random integer number between min [inclusive] and max [exclusive]
             NextIdleAllow = false;
         }
         else if (animPercent > .0f && animPercent < .5f && (NextIdleAllow == false))
@@ -45,25 +47,10 @@ public class IdleTest : MonoBehaviour {
             NextIdleAllow = true;
         }
 
-        //{
-            //int[] IdleAnims = new int[6] { idle00, idle01, idle02, idle03, idle04, idle05 };                //List of available variant anims
-
-            //int animLoopNum = (int)animState.normalizedTime;
-            //float animPercent = Mathf.Round(((animState.normalizedTime - animLoopNum) * 100f)) / 100f;     //round to DP2
-
-            //if (animPercent > .85f && canChangeState == true)       //crossfade after this percent
-            //{
-            //    while (CurrentIdleVariant == NextIdleVariant)
-            //        NextIdleVariant = UnityEngine.Random.Range(0, IdleAnims.Length);  //random select next transition
-            //    canChangeState = false;                             //stop state change until next crossfade
-            //    CurrentIdleVariant = NextIdleVariant;               //start selection of next random clip
-            //    anim.CrossFade(IdleAnims[NextIdleVariant], .3f, -1, float.NegativeInfinity);    //Crossfade to
-            //}
-            //else if (animPercent < .3f && canChangeState == false)  //arm for a new crossfade
-            //{
-            //    canChangeState = true;
-            //}
-        //}
+        nextIdleVector = idleAnimsList[NextIdle];       //get the vector for the nex Idle animation
+        currentIdleVector = Vector2.Lerp(currentIdleVector, nextIdleVector, Time.time * IdleSmooth);  //lerp for a smooth transition in 2D blendTree
+        anim.SetFloat("IdleRandA", currentIdleVector.x);
+        anim.SetFloat("IdleRandB", currentIdleVector.y);
     }
 
 }
