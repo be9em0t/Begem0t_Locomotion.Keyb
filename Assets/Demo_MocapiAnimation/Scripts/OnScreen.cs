ebf8bd7a48952c7cbaed62a4a04783d6e77d997f
be9em0t@gmail.com <be9em0t@gmail.com>
2014-05-20 10:10:06 +0300

    Settings WIP5. Error catching in UnityEditor. Experimenting with GUI designs

--- Assets/Demo_MocapiAnimation/Scripts/OnScreen.cs
+++ Assets/Demo_MocapiAnimation/Scripts/OnScreen.cs
@@ -9,6 +9,7 @@ namespace Mocapianimation
         public static Color guiTextColor;
         public static Color guiTitleColor;
 
+
         void Update()
         {
 
@@ -62,9 +63,9 @@ namespace Mocapianimation
             GUI.Label(new Rect(10, 80, 200, 120), "Reset Camera: Home, Gamepad 6", mainStyle);
 
             GUI.Label(new Rect(10, 120, 200, 120), "Gamepad", headerStyle);
-            GUI.Label(new Rect(10, 140, 200, 120), "Move: Left Stick Y", mainStyle);
-            GUI.Label(new Rect(10, 160, 200, 120), "Sidestep: LStick X", mainStyle);
-            GUI.Label(new Rect(10, 180, 200, 120), "Look Left/Right: RStick + button 4", mainStyle);
+            GUI.Label(new Rect(10, 140, 200, 120), "Move: Stick Y", mainStyle);
+            GUI.Label(new Rect(10, 160, 200, 120), "Sidestep: Alt + Stick X", mainStyle);
+            GUI.Label(new Rect(10, 180, 200, 120), "Look Left/Right: Ctrl + button 4", mainStyle);
             GUI.Label(new Rect(10, 200, 200, 120), "Alert: button 3", mainStyle);
             GUI.Label(new Rect(10, 220, 200, 120), "Sit Down: button 0", mainStyle);
 
@@ -79,10 +80,108 @@ namespace Mocapianimation
         }
 
         //Format Error Info panel
-        public static void ErrorInfo()
+        public static void ErrorInfoOFF()
         {
-            GUI.Box(new Rect((Screen.width - (Screen.width * .8f)) / 2, (Screen.height - (Screen.height * .5f)) / 2, Screen.width * .8f, Screen.height * .5f), "Input Manager Error. \n\n Please replace \n ProjectSettings\\InputManager.asset \n with the one contained in \n Assets\\Demo_MocapiAnimation\\InputManager_Demo.zip");
+            guiTextColor = new Color(0.94F, 0.6F, 0.2F, .92F);
+            guiTitleColor = new Color(1F, 1F, 1F, .85F);
+
+            GUIStyle infoStyle = new GUIStyle();
+            infoStyle.fontSize = 9;
+            infoStyle.font = GUI.skin.font;
+
+            GUIStyle headerStyleCentered = new GUIStyle();
+            headerStyleCentered.normal.textColor = guiTitleColor;
+            headerStyleCentered.fontSize = 15;
+            headerStyleCentered.alignment = TextAnchor.UpperCenter;
+            headerStyleCentered.font = GUI.skin.font;
+
+            GUIStyle mainStyleCentered = new GUIStyle();
+            mainStyleCentered.normal.textColor = guiTextColor;
+            mainStyleCentered.fontSize = 13;
+            mainStyleCentered.alignment = TextAnchor.UpperCenter;
+            mainStyleCentered.font = GUI.skin.font;
+
+            //GUI.BeginGroup(new Rect((Screen.width - (Screen.width * .8f)) / 2, (Screen.height - (Screen.height * .5f)) / 2, Screen.width * .8f, Screen.height * .5f));
+            GUI.Box(new Rect((Screen.width - (Screen.width * .8f)) / 2, (Screen.height - (Screen.height * .5f)) / 2, Screen.width * .8f, Screen.height * .5f), "");
+            GUI.Label(new Rect(Screen.width/2, Screen.height/2, 200, 120), "Input Manager Error.", headerStyleCentered);
+            GUI.Label(new Rect(10, 30, 200, 120), "Please replace \n ProjectSettings\\InputManager.asset \n with the one contained in \n Assets\\Demo_MocapiAnimation\\InputManager_Demo.zip", mainStyleCentered);
+
+            //GUI.EndGroup();
         }
 
+        public static void ErrorInfo()
+        {
+            //ui dimensions
+            int uiWidth = 420; 
+            int uiHeight = 180; 
+
+            //style definitions
+            Color guiTextColor = new Color(0.94F, 0.6F, 0.2F, .92F);
+            Color guiTitleColor = new Color(1F, 1F, 1F, .85F);
+
+            GUIStyle infoStyle = new GUIStyle();
+            infoStyle.fontSize = 9;
+            infoStyle.font = GUI.skin.font;
+
+            GUIStyle headerStyleCentered = new GUIStyle();
+            headerStyleCentered.normal.textColor = guiTitleColor;
+            headerStyleCentered.fontSize = 15;
+            headerStyleCentered.alignment = TextAnchor.UpperCenter;
+            headerStyleCentered.font = GUI.skin.font;
+            headerStyleCentered.wordWrap = true;
+
+            GUIStyle mainStyleCentered = new GUIStyle();
+            mainStyleCentered.normal.textColor = guiTextColor;
+            mainStyleCentered.fontSize = 13;
+            mainStyleCentered.alignment = TextAnchor.UpperCenter;
+            mainStyleCentered.font = GUI.skin.font;
+
+            // Group on the center of the screen
+            GUI.BeginGroup(new Rect(Screen.width / 2 - uiWidth/2, Screen.height / 2 - uiHeight/2, uiWidth, uiHeight));
+
+            // Box background.
+            GUI.Box(new Rect(0, 0, uiWidth, uiHeight), "Input Manager Error.");
+
+            // End the group we started above. This is very important to remember!
+            GUI.EndGroup();
+        }
+
+        public static Texture2D texture = new Texture2D(128, 128);
+
+        public static void ErrorInfo3()
+        {
+            //style definitions
+            Color guiTextColor = new Color(0.94F, 0.6F, 0.2F, .92F);
+            Color guiTitleColor = new Color(1F, 1F, 1F, .85F);
+            Color guiInfoBGColor = new Color(.4F, .4F, .4F, .6F);
+            Color guiErrorBGColor = new Color(1F, .4F, .4F, .6F);
+
+            GUIStyle headerStyleCentered = new GUIStyle();
+            headerStyleCentered.normal.textColor = guiTitleColor;
+            headerStyleCentered.fontSize = 15;
+            headerStyleCentered.alignment = TextAnchor.UpperCenter;
+            headerStyleCentered.font = GUI.skin.font;
+            headerStyleCentered.wordWrap = true;
+
+            //GUIStyle styleBG = new GUIStyle();
+            GUIStyle styleBG = new GUIStyle(GUI.skin.box);
+            styleBG.normal.background = MakeTex(2, 2, guiErrorBGColor);
+            GUI.Box(new Rect(100, 100, 100, 100), "test", styleBG);
+        }
+
+        private static Texture2D MakeTex(int width, int height, Color col) //courtesy of Benderlab
+        {
+            Color[] pix = new Color[width * height];
+            for (int i = 0; i < pix.Length; ++i)
+            {
+                pix[i] = col;
+            }
+
+            Texture2D result = new Texture2D(width, height);
+            result.SetPixels(pix);
+            result.Apply();
+            return result;
+        }
+    
     }
 }
